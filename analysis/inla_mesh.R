ad8bfaec8ed0e9f25fe2b4efa289f6e1ef26d4e1
nmmarquez <nmmarquez@gmail.com>
2017-02-23 14:39:25 -0600

    remove mesh creation file

--- analysis/inla_mesh.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-pacman::p_load(INLA, INSP, rgeos)
-
-head(mx.sp.df@data)
-centroids <- gCentroid(mx.sp.df, byid=TRUE)@coords
-mesh <- inla.mesh.create(centroids)
-spde <- inla.spde2.matern(mesh)
-
