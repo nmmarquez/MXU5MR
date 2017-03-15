f40d3c4bdd01384dcaa9c59ad879ddff684222a7
nmmarquez <nmmarquez@gmail.com>
2017-03-15 00:27:48 -0700

    [o[ adjusted

--- defunciones/pop_death_counts.R
+++ defunciones/pop_death_counts.R
@@ -5,61 +5,68 @@
 ################################################################################
 
 rm(list=ls())
-pacman::p_load(data.table, ggplot2, INSP, dplyr)
+pacman::p_load(data.table, ggplot2, INSP, dplyr, dtplyr)
 
 deathmd <- fread("~/Documents/MXU5MR/defunciones/outputs/mddeaths.csv")
-deaths  <- deathmd[,.N,by=list(ANIO_OCUR, SEXO, ENT_RESID, MUN_RESID, GEOID, EDADN)]
-deaths2 <- copy(deaths)
-deaths2[,YEAR:=ANIO_OCUR + 1]
-deaths2[,EDAD:=EDADN+1]
-deaths2[,DEATHS:=N]
-deaths2[,c("ENT_RESID", "MUN_RESID", "N", "ANIO_OCUR", "EDADN"):=NULL]
-deaths2[,COHORT:= YEAR - EDAD]
-deaths2
+deathmd[,YEAR:= ANIO_OCUR]
 
-pops <- fread("~/Documents/MXU5MR/nacimientos/outputs/deathlesspopcounts.csv")
-pops[,COHORT:= YEAR - EDAD]
-popsdgis <- fread("~/Documents/MXU5MR/nacimientos/outputs/deathlesspopcountsdgis.csv")
-popsdgis[,COHORT:= YEAR - EDAD]
+agedf <- fread("~/Documents/MXU5MR/nacimientos/outputs/age_groups.csv")
 
-for(i in 1:4){
-    subdeath <- subset(deaths2, EDAD == i)
-    subdeath[,c("YEAR", "EDAD"):=NULL]
-    pops <- as.data.table(left_join(pops, subdeath))
-    pops[is.na(DEATHS), DEATHS:=0]
-    pops[EDAD < i, DEATHS:=0]
-    pops[,N:=N - DEATHS]
-    pops[,DEATHS:=NULL]
-    popsdgis <- as.data.table(left_join(popsdgis, subdeath))
-    popsdgis[is.na(DEATHS), DEATHS:=0]
-    popsdgis[EDAD < i, DEATHS:=0]
-    popsdgis[,N:=N - DEATHS]
-    popsdgis[,DEATHS:=NULL]
+pops <- fread("~/Documents/MXU5MR/nacimientos/outputs/mdbirths.csv")
+pops[,YEAR := ANO_NAC]
+pops <- subset(pops, REGIS_DIFFN <= 1)
+popsdgis <- fread("~/Documents/MXU5MR/nacimientos/outputs/mdbirthsdgis.csv")
+popsdgis[,YEAR := ano]
+
+calc_cohort_pop <- function(popdf, deathdf, cohort, age_ver, intv=c(2011,2015)){
+    psub <- subset(popdf, YEAR == cohort, select=c(GEOID, YEAR))
+    psubagg <- psub[,.N, by=list(GEOID, YEAR)]
+    setnames(psubagg, "N", "POPULATION")
+    psubagg[,EDADN:=0]
+    psubagg[,DEATHS:=NA]
+    ageset <- subset(agedf, EDADV == age_ver)
+    dsub <- subset(deathdf, EDADV == age_ver, select=c(GEOID, YEAR, EDADN))
+    dsubagg <- dsub[,.N, by=list(GEOID, YEAR, EDADN)]
+    setnames(dsubagg, "N", "DEATHS")
+    year_curr <- cohort
+    for(i in 1:nrow(ageset)){
+        age <- ageset$EDADN[i]
+        deathage <- subset(dsubagg, EDADN == age & YEAR == year_curr)
+        year_curr <- ageset$ADV[i] + year_curr
+        popage <- subset(psubagg, EDADN == age)
+        popage[,DEATHS:=NULL]
+        demsubdf <- left_join(popage, deathage, by=c("GEOID", "YEAR", "EDADN"))
+        demsubdf[is.na(DEATHS), DEATHS:=0]
+        demsubdf2 <- copy(demsubdf)
+        demsubdf2[,POPULATION:=POPULATION - DEATHS]
+        demsubdf2[,DEATHS:=NA]
+        demsubdf2[,YEAR:=year_curr]
+        demsubdf2[,EDADN:=age+1]
+        demdf <- rbind(demsubdf, demsubdf2)
+        psubagg <- subset(psubagg, EDADN != age)
+        psubagg <- rbind(psubagg, demdf)
+    }
+    demfinal <- subset(psubagg, EDADN != age + 1 & YEAR <= intv[2] & 
+                           YEAR >= intv[1])
+    demfinal[,COHORT:=cohort]
+    demfinal
 }
 
-pops[,POPULATION:=N]
-popsdgis[,POPULATION2:=N]
-pops[,c("N", "COHORT"):=NULL]
-popsdgis[,c("N", "COHORT"):=NULL]
+calc_pops <- function(popdf, deathdf, age_ver){
+    demdf <- rbindlist(lapply(sort(unique(popdf$YEAR)), function(x) 
+        calc_cohort_pop(popdf, deathdf, x, age_ver)))
+    demdf[,EDADV:=age_ver]
+    demdf
+}
 
-deaths[,YEAR:=ANIO_OCUR]
-deaths[,EDAD:=EDADN]
-deaths[,DEATHS:=N]
-deaths[,c("ENT_RESID", "MUN_RESID", "N", "ANIO_OCUR", "EDADN"):=NULL]
-deaths
-
-demog <- subset(as.data.table(merge(pops, deaths, all=TRUE, 
-                                    by=c("SEXO", "GEOID", "YEAR", "EDAD"))),
-                YEAR <= 2015 & SEXO %in% c(1,2) & MUN_RESID != 999)
-demog <- as.data.table(merge(demog, popsdgis, all=TRUE, 
-                             by=c("SEXO", "ENT_RESID", "MUN_RESID", "GEOID", 
-                                  "EDAD", "YEAR")))
-demog <- subset(demog, SEXO %in% c(1,2) & MUN_RESID != 999 & ENT_RESID <= 32)
-summary(demog)
-
-demog[,COHORT:=YEAR - EDAD]
-subset(demog, COHORT == 2012 & GEOID == 1001)    
-
+demenegi <- rbindlist(lapply(1:3, function(x) calc_pops(pops, deathmd, x)))
+demdgis <- rbindlist(lapply(1:3, function(x) calc_pops(popsdgis, deathmd, x)))
+demdgis[,POPULATION2:=POPULATION]
+demdgis[,POPULATION:=NULL]
+demog <- left_join(demenegi, demdgis)
+demog[,MUN_RESID:=as.integer(substring(sprintf("%05d", GEOID), 3))]
+demog[,ENT_RESID:=as.integer(substring(sprintf("%05d", GEOID), 1, 2))]
+demog <- subset(demog, MUN_RESID != 999 & ENT_RESID <= 32)
 demog[is.na(DEATHS), DEATHS:=0]
 demog[is.na(POPULATION), POPULATION:=0]
 demog[is.na(POPULATION2), POPULATION2:=0]
@@ -67,7 +74,9 @@ demog[POPULATION<DEATHS,POPULATION:=DEATHS]
 demog[POPULATION2<DEATHS,POPULATION2:=DEATHS]
 summary(demog)
 
-muni_level <- demog[, lapply(list(POPULATION, DEATHS), sum), by=GEOID]
+subset(demog, COHORT == 2012 & GEOID == 1001 & EDADV == 1)
+
+muni_level <- subset(demog, EDADV == 1)[, lapply(list(POPULATION, DEATHS), sum), by=GEOID]
 setnames(muni_level, names(muni_level), c("GEOID","POPULATION", "DEATHS"))
 muni_level[,DEATHRT:=DEATHS/POPULATION*10**5]
 muni_level[,LNMXT:=log(DEATHRT)]
@@ -80,21 +89,20 @@ mx.sp.df@data <- left_join(mx.sp.df@data, muni_level)
 #spdf2leaf(mx.sp.df, "POPAVG", "Average<br>Population")
 #spdf2leaf(mx.sp.df, "LOGPOPAVG", "Average<br>Population<br>(Log)")
 #spdf2leaf(mx.sp.df, "DEATHRT", "Death Rate")
-#spdf2leaf(mx.sp.df, "LNMXT", "Death Rate<br>(Log)")
+spdf2leaf(mx.sp.df, "LNMXT", "Death Rate<br>(Log)")
 
 ggplot(data=mx.sp.df@data, aes(x=POPAVG, y=DEATHS)) +
     geom_point(alpha=.6, color="maroon4")
 
-head(demog)
-hist(log(demog$DEATHS + 1))
+hist(log(subset(demog, EDADV == 1)$DEATHS + 1))
 
-ya_df <- demog[,lapply(list(POPULATION, DEATHS, POPULATION2), sum), by=list(YEAR, EDAD)]
+ya_df <- subset(demog, EDADV == 1)[,lapply(list(POPULATION, DEATHS, POPULATION2), sum), by=list(YEAR, EDADN)]
 setnames(ya_df, names(ya_df), c("YEAR", "EDAD", "POPULATION", "DEATHS", "POPULATION2"))
 ya_df[,RT:=DEATHS/POPULATION]
 ya_df[,RT1000:=RT*10**3]
 subset(ya_df, EDAD == 1)
 
-yearly_df <- demog[,lapply(list(POPULATION, DEATHS, POPULATION2), sum), by=list(YEAR)]
+yearly_df <- subset(demog, EDADV == 1)[,lapply(list(POPULATION, DEATHS, POPULATION2), sum), by=list(YEAR)]
 setnames(yearly_df, names(yearly_df), c("YEAR", "POPULATION", "DEATHS", "POPULATION2"))
 yearly_df[,RT:=DEATHS/POPULATION]
 yearly_df[,RT1000:=RT*10**3]
@@ -102,16 +110,9 @@ yearly_df[,RT2:=DEATHS/POPULATION2]
 yearly_df[,RT21000:=RT2*10**3]
 yearly_df
 
-demog
-demogss <- demog[, lapply(list(POPULATION, DEATHS, POPULATION2), sum),
-                 by=list(ENT_RESID, MUN_RESID, GEOID, EDAD, YEAR)]
-setnames(demogss, names(demogss),
-         c("ENT_RESID", "MUN_RESID", "GEOID", "EDAD", "YEAR", "POPULATION", 
-           "DEATHS", "POPULATION2"))
-
-prod(sapply(c("GEOID", "YEAR", "EDAD"), function(x) length(unique(demogss[[x]]))))
+prod(sapply(c("GEOID", "YEAR", "EDADN"), function(x) length(unique(demog[[x]]))))
 
 unique(demog$GEOID)[!(unique(demog$GEOID) %in% as.integer(mx.sp.df@data$GEOID))]
 
-fwrite(demog, "~/Documents/MXU5MR/defunciones/outputs/sex_spec_demog.csv")
-fwrite(demogss, "~/Documents/MXU5MR/defunciones/outputs/demog.csv")
+
+fwrite(demog, "~/Documents/MXU5MR/defunciones/outputs/demog.csv")
