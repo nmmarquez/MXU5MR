5e98bc9af7e7f5e8ed4a5f4006edff2535f73b11
nmmarquez <nmmarquez@gmail.com>
2017-05-25 13:16:08 -0700

    download inegi births

---
+++ nacimientos/download_inegi.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+
+base <- "http://www.beta.inegi.org.mx/contenidos/proyectos/registros/vitales/"
+url_ <- paste0(base, "natalidad/microdatos/#/natalidad_base_datos_#_dbf.zip")
+save_dir <- "~/Documents/MXU5MR/nacimientos/data/inegi/"
+urls <- sapply(1985:2015, function(x) gsub("#", x, url_))
+
+tempfiles <- sapply(1:length(urls), function(x) tempfile())
+extr_dir <- tempdir()
+
+mapply(download.file, urls, tempfiles)
+
+# unzip los archivos
+lapply(tempfiles, unzip, exdir=extr_dir)
+
+
+# crear un objeto con todos los nuevo archivos
+tmpdirfiles <- list.files(extr_dir, full.names=TRUE)
+
+datafiles <- grep(".dbf", tmpdirfiles, value=TRUE)
+mvloc <- gsub(extr_dir, save_dir, datafiles)
+
+file.rename(datafiles, mvloc)
+
+sapply(tempfiles, unlink)
+unlink(extr_dir)
